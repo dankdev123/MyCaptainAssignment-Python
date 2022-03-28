a85bac5aeb543c4b75f3764d3d5ce0313391f231
antijo <98686183+antijo@users.noreply.github.com>
2022-03-28 17:29:14 +0400

    Add files via upload

---
+++ frequency counter.py
@@ -0,0 +1,14 @@
+from collections import Counter
+test_str = "Mississippi"
+  
+
+all_freq = {}
+  
+for i in test_str:
+    if i in all_freq:
+        all_freq[i] += 1
+    else:
+        all_freq[i] = 1
+test_str.sort()
+print ("count character :\n "
+                                        +  str(all_freq))
